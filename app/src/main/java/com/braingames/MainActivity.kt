ea1741a078c88bd6d46fe24b0a2edb9a3fb1b288
behzod <behzod96447@internet.ru>
2024-08-14 18:30:23 +0300

    first commit

---
+++ app/src/main/java/com/braingames/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.braingames
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import com.braingames.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initView()
+    }
+
+    private fun initView() {
+        val ncNavHost=supportFragmentManager.findFragmentById(R.id.ncNavHost) as NavHost
+        navController=ncNavHost.navController
+        navController
+    }
+}
