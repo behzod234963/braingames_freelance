2dd1fb3375e023a4d26751a40a8464de35579f6c
behzod <behzod96447@internet.ru>
2024-08-16 17:48:30 +0300

    first commit

---
+++ app/src/main/java/com/braingames/fragments/MenuFragment.kt
@@ -0,0 +1,34 @@
+package com.braingames.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.braingames.R
+import com.braingames.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    private lateinit var binding:FragmentMenuBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentMenuBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        binding.apply {
+
+        }
+    }
+}
